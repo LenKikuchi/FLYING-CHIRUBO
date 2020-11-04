71c14655399f42d95fc5ae4a7d6a35c85f76f322
Len Ichinose <psychonyannyan@gmail.com>
2020-11-04 23:23:51 +0900

    third commit
    create button

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    //ボタンを押した時に呼び出される関数
+    void PushButton()
+    {
+        //画面遷移の処理
+        SceneManager.LoadScene("GameSceneß");
+    }
+}
