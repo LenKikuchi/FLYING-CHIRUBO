3d1605ebb5e254a92c5c060181922a820f4863ae
Len Ichinose <psychonyannyan@gmail.com>
2020-11-11 18:54:20 +0900

    seventh commit
    add colider

---
+++ Assets/Scripts/RetryManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // リトライボタンを押したときにゲームをリスタートさせる
+    public void OnRetry()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
