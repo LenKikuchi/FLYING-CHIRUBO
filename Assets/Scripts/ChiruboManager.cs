498aa8cd3729c7d026c25646adc6eb2d364da34f
Len Ichinose <psychonyannyan@gmail.com>
2020-11-11 19:16:36 +0900

    eighth commit
    change sourcecode

--- Assets/Scripts/ChiruboManager.cs
+++ Assets/Scripts/ChiruboManager.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChiruboManager : MonoBehaviour
 {
     Rigidbody2D rb;
     float speed = 0.02f;
+
+    //ゲームオーバーキャンバスを格納する変数
+    public GameObject GameOver;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,5 +29,18 @@ public class ChiruboManager : MonoBehaviour
             //上向きの力を加える 引数にはx軸,y軸がそれぞれ格納される
             rb.velocity = new Vector2(0.0f, 5.0f);
         }
+    }
+
+    // ぶつかった時の処理
+    private void OnColliseionExit2D(Collision2D collision)
+    {
+        //ゲームオーバーキャンバスを出す
+        
+    }
+
+    // リトライボタンを押したときにゲームをリスタートさせる
+    public void OnRetry()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
